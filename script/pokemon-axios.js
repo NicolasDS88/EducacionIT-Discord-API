372e15f76bc90b1d6612bab58236fa9fce302877
NicolasDS <nicolas.dasilva88@gmail.com>
2022-07-11 16:03:37 -0300

    ok

--- script/pokemon-axios.js
+++ script/pokemon-axios.js
@@ -1,26 +1,26 @@
 const poke_container = document.getElementById('poke_container');
 const pokemons_number = 150;
 const colors = {
-	fire: '#FDDFDF',
-	grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	water: '#DEF3FD',
-	ground: '#f4e7da',
+	fire: '#C00505',
+	grass: '#39C01E',
+	electric: '#EEFF02',
+	water: '#2B87CE',
+	ground: '#644715',
 	rock: '#d5d5d4',
 	fairy: '#fceaff',
-	poison: '#98d7a5',
+	poison: '#C028AC',
 	bug: '#f8d5a3',
 	dragon: '#97b3e6',
 	psychic: '#eaeda1',
-	flying: '#F5F5F5',
+	flying: '#CECE5C',
 	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+	normal: '#87ABCE'
 };
 const main_types = Object.keys(colors);
 
 const fetchPokemons = async () => {
 	for (let i = 1; i <= pokemons_number; i++) {
-		await getPokemon(i);
+		await getPokemon(i); //give a promese -> await
 	}
 };
 
@@ -34,7 +34,6 @@ const getPokemon = async id => {
 function createPokemonCard(pokemon) {
 	const pokemonEl = document.createElement('div');
 	pokemonEl.classList.add('pokemon');
-
 	const poke_types = pokemon.types.map(type => type.type.name);
 	const type = main_types.find(type => poke_types.indexOf(type) > -1);
 	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
@@ -44,11 +43,30 @@ function createPokemonCard(pokemon) {
 
     //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png
 	const pokeInnerHTML = `
-        <div class="img-container" style="--clr:${color}">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-							pokemon.id
-						}.png" alt="${name}" />
+        <div class="img-container" >
+				<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+								pokemon.id
+							}.png" alt="${name}">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${
+								pokemon.id
+							}.png" alt="${name}"/>
+						</div>
+						
+						<div class="carousel-item">
+							<img class="d-block w-100" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+								pokemon.id
+							}.png" alt="${name}" />
+						</div>
+					</div>
+				</div>
+
         </div>
+
         <div class="info">
             <span class="number">#${pokemon.id
 							.toString()
@@ -69,20 +87,3 @@ fetchPokemons();
 
 
 
-
-
-
-
-
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
-
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
-
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
